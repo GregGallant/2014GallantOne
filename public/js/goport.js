eadf5d8d474207979f0526f900665f280bc2d82b
Greg Gallant <greggallant2005@gmail.com>
2013-01-10 19:07:28 -0500

    Fully loaded pages only... jquery work

--- public/js/goport.js
+++ public/js/goport.js
@@ -3,16 +3,28 @@
 $(document).ready(function() {
 
 
+
+    /* Hide the page until the grueling portfolio of eye bleeding work is fully loaded */
+    $(window).load(function() {
+        $(".preLoader").hide();
+        $(".portfolioContent").fadeIn(2000);
+    });
+
+
+    /* The Learn More button */
+
     $('.learnMore').click(function() {
 
         $.fancybox({'href' :'/light/1',
             //'title' : 'so far so good',
             'padding'       : 0,
-            'modal'     : false,
+            'modal'     : true,
             'autoScale'     : false,
-            'transitionIn'  : 'none',
+            'transitionIn'  : 'openEffect',
             'showLoading' : 'true',
             'transitionOut' : 'none',
+            'openEffect' : 'elastic',
+            'openSpeed' : 500,
             'width'     : '800px',
             'height'        : '600px',
             'maxWidth'     : '800px',
@@ -26,5 +38,5 @@ $(document).ready(function() {
 
     });
 
-
 });
+
