c0bd636f687913c3c8472de285705b06c8e390d4
Greg Gallant <greg@gallantone.com>
2013-02-14 23:48:24 -0500

    Register form stylized

--- module/Auth/src/Auth/Form/BaseUserForm.php
+++ module/Auth/src/Auth/Form/BaseUserForm.php
@@ -19,24 +19,24 @@ class BaseUserForm
 
     /**
      * @Annotation\Validator({"name":"EmailAddress"})
-     * @Annotation\Attributes({"type":"text", "class":"form_input"})
-     * @Annotation\Options({"label":"Email"})
+     * @Annotation\Attributes({"type":"text", "class":"form_input", "placeholder":"E-mail Address"})
+     * @Annotation\Options({"label":""})
      */
     public $email;
 
     /**
      * @Annotation\Filter({"name":"StringTrim"})
      * @Annotation\Validator({"name":"Regex", "options":{"pattern":"/^[a-zA-Z][a-zA-Z0-9_-]{0,24}$/"}})
-     * @Annotation\Attributes({"type":"password"})
+     * @Annotation\Attributes({"type":"password", "class":"form_input", "placeholder":"Password"})
      * @Annotation\Validator({"name":"StringLength", "options":{"min":"7", "max":"30"}})
-     * @Annotation\Options({"label":"Password"})
+     * @Annotation\Options({"label":""})
      */
     public $password;
 
 
 
     /**
-     * @Annotation\Attributes({"type":"submit"})
+     * @Annotation\Attributes({"type":"submit", "class":"submit_button"})
      */
     public $submit;
 
