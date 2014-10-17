db8e1edcbbedd73e78d59e6f570862610e1854e3
Greg Gallant <greggallant2005@gmail.com>
2014-10-17 12:02:45 -0400

    GO-1 Minor frontpage updates

--- GallantAPI/app/controllers/GallantController.php
+++ GallantAPI/app/controllers/GallantController.php
@@ -11,10 +11,18 @@ class GallantController extends BaseController {
      */
     public function index()
     {
+        return View::make('hello');
+    }
+
+/*
+    public function index()
+    {
 
     \View::addNamespace('angView', '/var/www/GallantOne/public/angPublic/views');
 
         return \View::make('angView::index');
     }
+ */
+       
 
 }
--- GallantOne/views/templates/fcontent.php
+++ GallantOne/views/templates/fcontent.php
@@ -11,7 +11,7 @@
             <p>    When it comes to design and corporate identity, the need for an engineer who understands the principles and intricities of fluid UI design becomes essential.</p>
 
             <p>These are but a few principles within GallantOne.com, home of software engineer and web producer Greg A. Gallant. Brining ideas to a technological life is the everyday challenge that keeps the digital world moving and what essentially keeps myself up to par as a professional company builder.</p>
-            <div id="viewButton"><a href="/portfolio/1"><img border="0" src="/images/one/button_view.png" /></a></div>
+            <div id="viewButton"><a href="#"><img border="0" src="/images/one/button_view.png" /></a></div>
         </div>
         <div class="aboutBox">
             <div class="gSubHeader"><img src="/images/one/flange.png"/>The Architect</div>
@@ -22,7 +22,7 @@
                 <br/>
                 <br/>
                 Specializations and experience include the building of several small technology companies using a multitude of programming languages including <a target="springsource" href="http://springsource.org">Java Spring MVC 3</a>, <a target="php" href="http://www.php.net">PHP</a> <a href="http://www.laravel.com">Laravel</a>, <a href="www.angularjs.org">Angular JS</a>, <a href="http://framework.zend.com">Zend 2 Framework</a> and <a href="http://ellislab.com/codeigniter">CodeIgniter</a>, <a href="http://www.python.org">Python</a>, Javascript, and <a href="http://jquery.com">JQuery</a>. This includes database programming using <a href="http://www.mysql.com">MySQL</a>, <a href="http://www.oracle.com">Oracle</a>, and <a href="http://www.postgresql.org">PostGreSQL</a>.</p>
-            <div id="contactButton"><a href="/contact"><img border="0" src="/images/one/button_contact.png" /></a></div>
+            <div id="contactButton"><a href="#"><img border="0" src="/images/one/button_contact.png" /></a></div>
         </div>
     </div>
 </div>
