ba99dd0a0235b6f330a1ca3032b3bdf96e572198
Greg Gallant <greggallant2005@gmail.com>
2014-10-20 17:35:45 -0400

    API wrapper needs serious optimization

--- GallantOne/api/portfolio.php
+++ GallantOne/api/portfolio.php
@@ -1,30 +1,87 @@
 <?php
 
-    $url = "http://api.gallantone.com/portfolio";
 
-     // create curl resource 
-    $ch = curl_init(); 
+class PortfolioAPI 
+{
+    /* API request file */
+
+    protected $ch;  // curl handle
+
+    /* Use this later */
+    public function __construct() 
+    {
+        //$ch = $this->initCurl();
+    }
+
+    /**
+     * Get all Portfolio index
+     */
+    public function getAllPortfolio() 
+    {
+    
+       $url = "http://api.gallantone.com/portfolio";
+
+       $this->ch = $this->initCurl();
+
+       // $output contains the output string 
+       $output = curl_exec($this->ch); 
+
+       // close curl resource to free up system resources 
+       curl_close($this->ch);
+
+       return $output;
+    }
 
 
-   // set url 
-   curl_setopt($ch, CURLOPT_URL, $url); 
+    /**
+     * Get one.
+     */
+    public function getPortfolio($id) 
+    {
+       $url = "http://api.gallantone.com/portfolio/"+$id;
 
-   //return the transfer as a string 
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+       $this->ch = $this->initCurl();
 
-   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+       // $output contains the output string 
+       $output = curl_exec($this->ch);
 
-   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-   curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-   curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+       // close curl resource to free up system resources 
+       curl_close($this->ch);
+
+       return $output;  
+    }
+
+    /**
+     * Curl setup
+     */
+    private function initCurl() 
+    {
+        // create curl resource 
+        $ch = curl_init(); 
+
+        // set url 
+        curl_setopt($ch, CURLOPT_URL, $url); 
+
+        //return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+        return $ch;
+    }
+
+}
 
 
-   // $output contains the output string 
-   $output = curl_exec($ch); 
+$port = new PortfolioAPI;
 
-   // close curl resource to free up system resources 
-   curl_close($ch);
-
-   echo $output;
+// if querystring is this...
+echo $port->getAllPortfolio();
+// else if
+// else 
